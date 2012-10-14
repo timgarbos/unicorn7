6d6c7b22a51fcb6571596c4901ba230f1098773a
timgarbos <timgarbos@gmail.com>
2012-10-14 12:41:21 +0200

    Fixtures for initial data

---
+++ unicorn/fixtures/fixtures.sh
@@ -0,0 +1,4 @@
+./manage.py loaddata unicorn/fixtures/sites.json
+./manage.py loaddata unicorn/fixtures/socialaccount.json
+./manage.py loaddata unicorn/fixtures/games.json
+./manage.py loaddata unicorn/fixtures/oembed.json
